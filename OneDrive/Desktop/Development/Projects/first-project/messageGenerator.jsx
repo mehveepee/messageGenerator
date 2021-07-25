4a898424a4d3103cd4d386f1de89ea1348888beb
mehveepee <mehveepee@gmail.com>
2021-07-25 19:51:47 -0400

    generate 3 traits and combine them all into a single sentence.

--- OneDrive/Desktop/Development/Projects/first-project/messageGenerator.jsx
+++ OneDrive/Desktop/Development/Projects/first-project/messageGenerator.jsx
@@ -10,3 +10,32 @@ var myArray = [
 var randomIdeal = myArray[Math.floor(Math.random()*myArray.length)];
 
 console.log(randomIdeal);
+
+var bondArray = [
+    "I would die to recover an ancient relic of my faith that was lost long ago.",
+ "I will someday get revenge on the corrupt temple hierarchy who branded me a heretic.",
+ "I owe my life to the priest who took me in when my parents died.",
+ "Everything I do is for the common people. 5 I will do anything to protect the temple where I served.",
+ "I seek to preserve a sacred text that my enemies consider heretical and seek to destroy"
+];
+
+var randomBond = bondArray[Math.floor(Math.random()*bondArray.length )];
+
+console.log(randomBond);
+
+var flawArray = [
+    "I judge others harshly, and myself even more severely.",
+ "I put too much trust in those who wield power within my temple’s hierarchy.",
+ "My piety sometimes leads me to blindly trust those that profess faith in my god.",
+ "I am inflexible in my thinking.",
+ "I am suspicious of strangers and expect the worst of them.",
+ "Once I pick a goal, I become obsessed with it to the detriment of everything else in my life."
+];
+
+var randomFlaw = flawArray[Math.floor(Math.random()*flawArray.length)];
+
+console.log(randomFlaw);
+
+var response = `My name is Bruce and my main ideal is ` + randomIdeal + `. I have a bond that would be best described as: ` + randomBond + ` My main flaw is ` + randomFlaw;
+
+console.log(response);
